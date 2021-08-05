9e1d40c7d6b4f97a6841ad1da09d506a852ae6a1
LuisAGP <angel.lagp98@gmail.com>
2021-08-04 20:10:53 -0500

    Auto pull configured

--- main/views.py
+++ main/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import git
+import subprocess
 
 
 @csrf_exempt
 def update(request):
-    repo = git.Repo('/home/opi/django_project')
-    repo.remotes.origin.pull()
+
+    run("cd /home/opi/django_project")
+    run("git pull origin master")
+    run("service apache2 restart")
+    run("cd /home/opi/django_project/.git/objects")
+    run("chown -R opi:opi *")
 
     return JsonResponse({'message':'OK!'})
+
+
+def run(cmd):
+    msg = "Done!"
+    try:
+
+        psw = 'L1998luis'
+        command = cmd.split()
+
+        cmd1 = subprocess.Popen(['echo',psw], stdout=subprocess.PIPE)
+        cmd2 = subprocess.Popen(['sudo','-S'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+    
+    except Exception:
+        msg = "Failed!"
+    
+    return msg
 
     
 
