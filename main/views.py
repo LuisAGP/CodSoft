d9c2476904a8716840480c4852c40920954e1120
LuisAGP <angel.lagp98@gmail.com>
2021-08-04 22:33:26 -0500

    Autoupdate

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import git
+import subprocess
 
 
 @csrf_exempt
@@ -9,8 +10,17 @@ def update(request):
 
     msg = {'msg':'done!'}
 
-    repo = git.Repo('/home/opi/django_project')
-    repo.remotes.origin.pull()
+    psw = 'L1998luis'
+    command1 = 'service apache2 restart'.split()
+    command2 = 'sudo chmod -R 775 /home/opi/django_project'.split()
+
+    cmd1 = subprocess.Popen(['echo',psw], stdout=subprocess.PIPE)
+    cmd2 = subprocess.Popen(['sudo','-S'] + command2, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+
+    subprocess.call('git pull origin master', shell=True, cwd="/home/opi/django_project")
+
+    cmd2 = subprocess.Popen(['sudo','-S'] + command1, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+    cmd2 = subprocess.Popen(['sudo','-S'] + command2, stdin=cmd1.stdout, stdout=subprocess.PIPE)
 
     return JsonResponse(msg)
 
