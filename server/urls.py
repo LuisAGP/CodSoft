ef11d7985426e7af0e1c98d457dbeffe58f49541
Luis <luis.lagp4598@gmail.com>
2021-08-05 22:11:44 -0500

    Reapiring autoupdate

--- server/urls.py
+++ server/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cdsf/', include('main.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
-    #path('', include('main.urls'))
 ]
