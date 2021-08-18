65b27fba6cd065de2a23430aff063a1265cc671f
Luis <luis.lagp4598@gmail.com>
2021-08-17 21:32:28 -0500

    Navbar Done!

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,17 +3,28 @@ import '../../static/css/header.css'
 import menuIcon from '../../static/images/menu.svg'
 
 const Header = () => {
+
+
+    const showMenu = () => {
+
+        let menu = document.getElementById('menu');
+        menu.className = menu.className === "menu-tools hide" ? "menu-tools show" : "menu-tools hide";
+        menu.focus()
+
+    }
+
+
     return (
         <div className="header">
             <div className="menu-icon">
-                <img src={menuIcon} alt="" />
+                <img src={menuIcon} alt="Menu_icon" onClick={ () => showMenu()}/>
             </div>
 
             <div className="options">
                 <a href="#">Get Started</a>
             </div>
 
-            <div className="menu-tools">
+            <div className="menu-tools hide" id="menu" tabIndex="0" onBlur={() => document.getElementById('menu').className="menu-tools hide"}>
                 <h2>CodTools</h2>
 
                 <div className="menu-options">
