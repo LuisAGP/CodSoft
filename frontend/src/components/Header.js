ac5274863767ec66a14f0fa9e8bacde3f5ac1d68
Luis <luis.lagp4598@gmail.com>
2021-08-17 20:39:28 -0500

    Changing the style of navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,12 +10,20 @@ const Header = () => {
             </div>
 
             <div className="options">
-                <a href="#">Modal Maker</a>
-                <a href="#">Box-Shadow</a>
-                <a href="#">Buttons</a>
-                <a href="#">General Ajax</a>
-                <a href="#">Image Reziser</a>
-                <a href="#">Table Generator</a>
+                <a href="#">Get Started</a>
+            </div>
+
+            <div className="menu-tools">
+                <h2>CodTools</h2>
+
+                <div className="menu-options">
+                    <a href="#">Modal Maker</a>
+                    <a href="#">Box-Shadow Calculator</a>
+                    <a href="#">Button generator</a>
+                    <a href="#">General Ajax</a>
+                    <a href="#">Image Reziser</a>
+                    <a href="#">Table Generator</a>
+                </div>
             </div>
         </div>
     )
