ded4e9d54fc8d358f7a962eab578e6d1a0d20f12
LuisAGP <angel.lagp98@gmail.com>
2021-08-30 23:04:36 -0500

    auto migrations

--- server/wsgi.py
+++ server/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+call_command("makemigrations")
+call_command("migrate")
