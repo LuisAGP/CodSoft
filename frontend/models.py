3823abc0397f58abcca235f9e790e701a431c65c
LuisAGP <angel.lagp98@gmail.com>
2021-08-30 22:01:07 -0500

    adding PostgreSQL

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.db.models.fields import IntegerField
 
 # Create your models here.
+class Test(models.Model):
+    id_test = models.IntegerField()
--- server/settings.py
+++ server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .dbconnection import DB
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,12 +87,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DB
 
 
 # Password validation
