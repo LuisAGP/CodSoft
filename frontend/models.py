3ebbc99c7e3ef647a3ce1683d4c952184521c800
Luis <luis.lagp4598@gmail.com>
2021-08-30 22:24:31 -0500

    Testing remote migrations

--- frontend/models.py
+++ frontend/models.py
@@ -4,3 +4,4 @@ from django.db.models.fields import IntegerField
 # Create your models here.
 class Test(models.Model):
     id_test = models.IntegerField()
+    text = models.CharField(max_length=255, null=True)
