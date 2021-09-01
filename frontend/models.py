9df24dd164d0a440ef8a50ed0d2640f0d3410151
LuisAGP <angel.lagp98@gmail.com>
2021-08-31 21:42:56 -0500

    Solving problems

--- frontend/models.py
+++ frontend/models.py
@@ -11,9 +11,5 @@ class Test(models.Model):
     c = models.BooleanField(default=True, null=True)
     d = models.IntegerField(default=5)
     e = models.CharField(max_length=50, default=True)
-<<<<<<< HEAD
-    f = models.DateField(default=timezone.now)
-=======
     f = models.DateField(default=timezone.now, null=True)
     g = models.IntegerField(null=True, default=2)
->>>>>>> 12ac2fb90a6d735b0e0c06dde92270727a69fecf
