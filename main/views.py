66013ada4fb3743f62a67f20633e8be08cae86fd
Luis <luis.lagp4598@gmail.com>
2021-09-03 21:41:17 -0500

    sad

--- main/views.py
+++ main/views.py
@@ -11,9 +11,6 @@ def update(request):
     msg = {'msg':'done!'}
     
     requests.get('http://localhost:8001/gitPull/')
-    
-    subprocess.Popen("python3 /home/opi/django_project/manage.py makemigrations".split())
-    subprocess.Popen("python3 /home/opi/django_project/manage.py migrate".split())
 
     return JsonResponse(msg)
 
