fc5b60811970cdf110cab9bd7af2102b5e2a9614
LuisAGP <angel.lagp98@gmail.com>
2021-09-03 21:20:59 -0500

    Test

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import requests
 import subprocess
 
 
@@ -10,10 +9,8 @@ def update(request):
 
     msg = {'msg':'done!'}
     
-    requests.get('http://localhost:8001/gitPull/')
-    
-    subprocess.Popen("python3 /home/opi/django_project/manage.py makemigrations".split())
-    subprocess.Popen("python3 /home/opi/django_project/manage.py migrate".split())
+    command1 = '/home/opi/Desktop/Python/command.sh'.split()
+    cmd1 = subprocess.call(command1)
 
     return JsonResponse(msg)
 
