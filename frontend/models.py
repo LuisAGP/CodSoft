c56252b49f0b48a7a19ad688ec5d3b76d947dcee
LuisAGP <angel.lagp98@gmail.com>
2021-09-04 00:39:54 -0500

    Fixing autoupdate and automigrate

--- frontend/models.py
+++ frontend/models.py
@@ -4,13 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Test(models.Model):
     id_test = models.IntegerField()
-    text = models.CharField(max_length=255, null=True)
-    a = models.CharField(max_length=255, null=True)
-    phone = models.CharField(max_length=10, null=True, unique=True)
-    email = models.EmailField(null=True)
-    age = models.IntegerField(null=True)
+    a = models.CharField(max_length=30, null=True)
+    aa = models.CharField(max_length=30, null=True)
     b = models.CharField(max_length=30, null=True)
-    c = models.CharField(max_length=30, null=True)
-    d = models.CharField(max_length=30, null=True)
-    e = models.CharField(max_length=30, null=True)
-    f = models.CharField(max_length=30, null=True)
+    bb = models.CharField(max_length=30, null=True)
--- main/views.py
+++ main/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
+import subprocess
 
 
 @csrf_exempt
 def update(request):
 
     msg = {'msg':'done!'}
-    requests.get('http://localhost:8001/gitPull/')
+    #requests.get('http://localhost:8001/gitPull/')
+    command1 = '/home/opi/Desktop/Python/command.sh'.split()
+    subprocess.call(command1)
     return JsonResponse(msg)
 
     
