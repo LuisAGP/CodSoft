18233965601e654bdea5b4aaaf4c27e012fbddaf
LuisAGP <angel.lagp98@gmail.com>
2021-09-03 21:53:17 -0500

    Simpre si

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
-import subprocess
 
 
 @csrf_exempt
@@ -10,10 +9,7 @@ def update(request):
 
     msg = {'msg':'done!'}
     
-    #requests.get('http://localhost:8001/gitPull/')
-    
-    command1 = '/home/opi/Desktop/Python/command.sh'.split()
-    cmd1 = subprocess.call(command1)
+    requests.get('http://localhost:8001/gitPull/')
 
     return JsonResponse(msg)
 
