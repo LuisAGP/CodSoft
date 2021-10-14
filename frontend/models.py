77290981ce3beebf10474ea10e8bb289cad11680
LuisAGP <luis.lagp4598@gmail.com>
2021-10-13 20:32:17 -0500

    Fixing problem in production

--- frontend/models.py
+++ frontend/models.py
@@ -3,19 +3,6 @@ from django.db.models.base import Model
 from django.utils import timezone
 
 # Create your models here.
-class Test(models.Model):
-    id_test = models.IntegerField()
-    a = models.CharField(max_length=30, null=True)
-    aa = models.CharField(max_length=30, null=True)
-    b = models.CharField(max_length=30, null=True)
-    bb = models.CharField(max_length=30, null=True)
-    c = models.CharField(max_length=30, null=True)
-    field = models.CharField(max_length=30, null=True)
-    field2 = models.CharField(max_length=30, null=True)
-    field3 = models.CharField(max_length=30, null=True)
-    field4 = models.CharField(max_length=40, null=True)
-    field5 = models.CharField(max_length=30, null=True)
-
 
 
 class Folder(models.Model):
